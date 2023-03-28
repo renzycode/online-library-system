53fb31c1cfc5df64d3db8678ee32c9ea1c73556e
renzycode <renzyjohnm1@gmail.com>
2023-03-28 21:19:06 +0800

    upfate

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel = "icon" href="../assets/image/logo.png" type = "image/x-icon">
     <title>Online Library System</title>
 
     <!-- Custom fonts for this template-->
