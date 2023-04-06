75673ef0415e86ef63645cc200aac139f0cd8f3e
renzycode <renzyjohnm1@gmail.com>
2023-04-07 00:42:32 +0800

    update

--- includes/conn.php
+++ includes/conn.php
@@ -1,15 +1,15 @@
 <?php
 
-// online db
-// $host = 'b64jrih3qbsoq7z8qhd7-mysql.services.clever-cloud.com';
-// $user = 'ubltee2psvzbkjll';
-// $password = 'N8o0g8xlGV5l9IFVr9c0';
-// $db = 'b64jrih3qbsoq7z8qhd7';
+//online db
+$host = 'bpoks2m9vgtiyskrronx-mysql.services.clever-cloud.com';
+$user = 'u4y7dmqjbvkykhsr';
+$password = 'JX2fJND5b2Ztt0i9bqL0';
+$db = 'bpoks2m9vgtiyskrronx';
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$db = 'onlinelibrarysystem';
+//$host = 'localhost';
+//$user = 'root';
+//$password = '';
+//$db = 'onlinelibrarysystem';
 
 try {
 	$pdo = new PDO("mysql:host=".$host.";dbname=".$db.";charset=UTF8", $user, $password);
@@ -20,5 +20,3 @@ try {
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
-
-mysql://ubltee2psvzbkjll:N8o0g8xlGV5l9IFVr9c0@b64jrih3qbsoq7z8qhd7-mysql.services.clever-cloud.com:3306/b64jrih3qbsoq7z8qhd7
