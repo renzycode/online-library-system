f09e7a11e068495b59e26a52356c9b2709cff445
renzycode <renzyjohnm1@gmail.com>
2023-04-26 22:17:10 +0800

    aDDED

--- includes/conn.php
+++ includes/conn.php
@@ -1,11 +1,22 @@
 <?php
 
-//online db
+//online db 1
 
-$host = 'bpoks2m9vgtiyskrronx-mysql.services.clever-cloud.com';
-$user = 'u4y7dmqjbvkykhsr';
-$password = 'JX2fJND5b2Ztt0i9bqL0';
-$db = 'bpoks2m9vgtiyskrronx';
+//$host = 'bpoks2m9vgtiyskrronx-mysql.services.clever-cloud.com';
+//$user = 'u4y7dmqjbvkykhsr';
+//$password = 'JX2fJND5b2Ztt0i9bqL0';
+//$db = 'bpoks2m9vgtiyskrronx';
+
+
+
+//online db 2
+
+$host = 'b2hqvttkuzvtui0fat1z-mysql.services.clever-cloud.com';
+$user = 'u80tegkgvgyqohka';
+$password = 'tIeOIgfgsNLfzTi8OJ1V';
+$db = 'b2hqvttkuzvtui0fat1z';
+
+
 
 //$host = 'localhost';
 //$user = 'root';
